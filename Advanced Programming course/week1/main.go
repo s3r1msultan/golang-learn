e8f10fb65ec5a82efd0c7f0786b027f76c73c1bd
Hamlethan27 <hamlethan1999@gmail.com>
2023-12-12 23:39:34 +0600

    started week1

---
+++ Advanced Programming course/week1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(resp.Body)
+
+}
